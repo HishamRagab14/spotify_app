fe394c349da73db06de40b8b5c2a7e659d92a8d2
HishamRagab14 <hishamragab14@yahoo.com>
2025-02-16 18:38:39 +0200

    create basic app button in common folder and finish get started page

---
+++ lib/common/widgets/buttons/basic_app_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class BasicAppButton extends StatelessWidget {
+  const BasicAppButton({super.key, this.onPressed, required this.text, this.height});
+  final void Function()? onPressed;
+  final String text;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size.fromHeight(height?? 80),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Color(0xFFF6F6F6),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_app/common/widgets/buttons/basic_app_button.dart';
 import 'package:spotify_app/core/utils/assets.dart';
 
 class GetStarted extends StatelessWidget {
@@ -14,9 +15,9 @@ class GetStarted extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 40, vertical: 60),
             decoration: BoxDecoration(
               image: DecorationImage(
-                fit: BoxFit.fitHeight,
+                fit: BoxFit.cover,
                 image: AssetImage(
-                  Assets.imagesEminemtany,
+                  Assets.imagesAriana,
                 ),
               ),
             ),
@@ -31,7 +32,7 @@ class GetStarted extends StatelessWidget {
                   'Enjoy listening to music',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    color: Colors.black,
+                    color: Color(0xFFDADADA),
                     fontSize: 25,
                   ),
                 ),
@@ -42,14 +43,26 @@ class GetStarted extends StatelessWidget {
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
                   style: TextStyle(
                     fontWeight: FontWeight.w500,
-                    color: Colors.black45,
+                    color: Color(0xFF797979),
                     fontSize: 17,
                   ),
                   textAlign: TextAlign.center,
                 ),
+                SizedBox(
+                  height: 40,
+                ),
+                BasicAppButton(
+                  text: 'Get started',
+                  onPressed: () {
+                    // Navigator.pushReplacement(context, newRoute);
+                  },
+                ),
               ],
             ),
           ),
+          Container(
+            color: const Color.fromRGBO(0, 0, 0, 0.15),
+          )
         ],
       ),
     );
