c414e9b9fd14f106939b28476ae7754d91f1e1d8
HishamRagab14 <hishamragab14@yahoo.com>
2025-02-18 07:11:38 +0200

    install firebase core and build a spotify project on firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/configs/theme/app_theme.dart';
+import 'package:spotify_app/firebase_options.dart';
 import 'package:spotify_app/presentation/choose_mode/bloc/theme_cubit/theme_cubit.dart';
 import 'package:spotify_app/presentation/splash/pages/splash_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorageDirectory.web
---
+++ lib/presentation/authentication/signin/pages/signin_body.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_app/common/widgets/buttons/basic_app_button.dart';
+import 'package:spotify_app/common/widgets/social_icons_row.dart';
+import 'package:spotify_app/core/configs/theme/app_colors.dart';
+import 'package:spotify_app/presentation/authentication/signup/pages/or_divider.dart';
+import 'package:spotify_app/presentation/authentication/signup/sign_up.dart';
+
+class SigninBody extends StatelessWidget {
+  const SigninBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _registerText(),
+              SizedBox(height: 15),
+              _supportTextField(),
+              SizedBox(height: 20),
+              _emailField(context),
+              SizedBox(height: 15),
+              _passwordField(context),
+              SizedBox(height: 30),
+              BasicAppButton(
+                text: 'Sign In',
+                onPressed: () {},
+              ),
+              SizedBox(height: 30),
+              OrDivider(),
+              SizedBox(height: 20),
+              // Row(
+              //   crossAxisAlignment: CrossAxisAlignment.end,
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     Padding(
+              //       padding: const EdgeInsets.only(right: 30),
+              //       child: GestureDetector(
+              //           child: SvgPicture.asset(Assets.imagesGoogleicon)),
+              //     ),
+              //     GestureDetector(
+              //       child: SvgPicture.asset(
+              //         Assets.imagesAppleicon,
+              //         height: 34,
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              SocialIconsRow(),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              _signInText(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _registerText() {
+    return Text(
+      'Sign In',
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: 'Enter Username Or Email',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+      ).applyDefaults(
+        Theme.of(context).inputDecorationTheme,
+      ),
+    );
+  }
+
+  Widget _supportTextField() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('If you need any support '),
+        GestureDetector(
+          onTap: () {},
+          child: Text(
+            'click here',
+            style: TextStyle(
+              color: AppColors.primaryColor,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _signInText(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Not A Member ?  ',
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 14,
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => SignupPage()));
+          },
+          child: Text(
+            'Register Now?',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF288CE9),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
